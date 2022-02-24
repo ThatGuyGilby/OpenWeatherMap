d49bbdbbfdf4bbb4c760297078d4dfd1513168fe
DalePBernardo <dalepb18@gmail.com>
2022-02-24 10:42:40 +0000

    Refactoring Injector class

--- src/main/java/com/magicpythons/owm/Injector.java
+++ src/main/java/com/magicpythons/owm/Injector.java
@@ -1,24 +1,18 @@
 package com.magicpythons.owm;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.magicpythons.owm.weather.Weather;
-
-import java.io.IOException;
-import java.net.URL;
 import java.net.http.HttpResponse;
 
 public class Injector {
 
-    public DataTransferObject convertResponseToDTO(HttpResponse response){
-        ObjectMapper objectMapper = new ObjectMapper();
-        DataTransferObject dto = new DataTransferObject();
+    private ConnectionManager connectionManager;
 
-        try {
-            Weather weather = objectMapper.readValue(new URL("http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=c977119cbca2b2f41098fbaf53a315ae"), Weather.class);
-            // TODO - set DTO attributes
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public Injector(ConnectionManager connectionManager) {
+        this.connectionManager = connectionManager;
+    }
+
+    public DataTransferObject convertResponseToDTO(HttpResponse<String> response){
+        DataTransferObject dto = new DataTransferObject();
+        
         return null;
     }
 }
