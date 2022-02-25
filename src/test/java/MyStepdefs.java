2230590f6ca73860a2be3711c056f4e8c9a1cf34
andymurray32 <andrewmurray5@outlook.com>
2022-02-25 10:15:36 +0000

    Few scenarios

--- src/test/java/MyStepdefs.java
+++ src/test/java/MyStepdefs.java
@@ -1,23 +1,59 @@
+import com.magicpythons.owm.ConnectionManager;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Assertions;
+
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class MyStepdefs {
+    private static ConnectionManager connectionManager;
+    private static HttpClient httpClient;
+
+    HttpResponse<String> httpResponse;
     @Given("I have sent an HTTP request")
     public void iHaveSentAnHTTPRequest() {
+        connectionManager = new ConnectionManager();
+        httpClient = HttpClient.newHttpClient();
         System.out.println("Hello");
     }
 
     @And("it is received by the server")
     public void itIsReceivedByTheServer() {
+        HttpRequest httpRequest = connectionManager.getRequest("https://api.openweathermap.org/data/2.5/weather?q=London&appid=");
+        HttpClient httpClient = HttpClient.newHttpClient();
+        httpResponse = connectionManager.getResponse(httpClient, httpRequest);
     }
 
     @When("it is accepted")
     public void itIsAccepted() {
+        httpResponse.statusCode();
     }
 
-    @Then("the Status code should be {int}XX")
-    public void theStatusCodeShouldBeXX(int arg0) {
+    @Then("the Status code should be valid")
+    public void the_status_code_should_be_valid() {
+        Assertions.assertTrue(httpResponse.statusCode()> 199 &&httpResponse.statusCode()< 203);
     }
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+    @Given("My request has been accepted")
+    public void iHaveMadeASuccessfulRequest() {
+        Assertions.assertTrue(httpResponse.statusCode()> 199 &&httpResponse.statusCode()< 203);
+    }
+
+    @When("I receive a response")
+    public void iReceiveAResponse() {
+        Assertions.assertFalse(httpResponse.body().isEmpty());
+    }
+
+    @Then("The JSON response is converted to String")
+    public void responseTypeIsString() {
+        Assertions.assertTrue(httpResponse instanceof HttpResponse<String>);
+    }
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 }
+
--- src/test/resources/weather.feature
+++ src/test/resources/weather.feature
@@ -1,24 +1,25 @@
-Feature: Successful connection
+Feature: Testing Framework
   @ValidConnection
   Scenario: Accepted and received a request
     Given I have sent an HTTP request
     And it is received by the server
     When it is accepted
-    Then the Status code should be 2XX
+    Then the Status code should be valid
 
-#  @InvalidURL
-#  Scenario: Invalid URL Entered
-#  Given I have sent an HTTP request
-#  And it is received by the server
-#  When not found
-#  Then the Status code should be 404
-#
-#  @
-#  Scenario: Request is answered with JSON
-#    Given I have made a successful request
-#    And the request is answered
-#    When I receive a response
-#    Then response type is JSON
+Feature: Getting response
+  @RequestAnswered
+ Scenario: Response is given and taken as String
+   Given I have made a successful request
+    When I get a response
+   Then response type is HttpResponse<String>
+
+Feature: Converting String to Classes
+  @RequestAnswered
+  Scenario: Injecting objects into Classes
+    Given I have received a response
+    When I process this data
+    Then Each Class should hold the relevant data
+
 
 
 
